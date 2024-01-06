e123b2160dc5e454d8ec937a2213e4feb758fea9
msh63 <mohammad.shafe@gmail.com>
2024-01-07 01:24:07 +1300

    create accounts and config models fix errors

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 class UserManager(BaseUserManager):
-    def Create_User(self, first_name, last_name, username, email, password=None):
+    def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError('User must have an email address')
 
@@ -12,11 +12,10 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an username')
 
         user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
 
         )
         user.set_password(password)
@@ -24,10 +23,10 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, first_name, last_name, username, email, password=None):
-        user = self = self.create_superuser(
-
+        user = self.create_user(
             email=self.normalize_email(email),
             username=username,
+            password=password,
             first_name=first_name,
             last_name=last_name,
 
@@ -40,7 +39,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser):
+class User (AbstractBaseUser):
     RESTAURANT = 1
     CUSTOMER = 2
     ROLE_CHOICE = (
@@ -50,11 +49,10 @@ class User(AbstractBaseUser):
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    user_name = models.CharField(max_length=50, unique=True)
-    email = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=12, unique=True)
-    role = models.PositiveSmallIntegerField(
-        choice=ROLE_CHOICE, blank=True, null=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
 
     # required field
     date_joined = models.DateTimeField(auto_now_add=True)
@@ -68,6 +66,8 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects=UserManager()
 
     def __str__(self):
         return self.email
