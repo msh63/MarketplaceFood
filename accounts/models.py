2374e1bbee6c9b6ce891b71ea3c1be61d830ea3b
msh63 <mohammad.shafe@gmail.com>
2024-01-20 21:43:22 +1300

    vendor profile and validation

--- accounts/models.py
+++ accounts/models.py
@@ -97,8 +97,7 @@ class UserProfile(models.Model):
         upload_to='users/profile_picture', blank=True, null=True)
     cover_photo = models.ImageField(
         upload_to='users/cover_photos', blank=True, null=True)
-    address_line1 = models.CharField(max_length=50, blank=True, null=True)
-    address_line2 = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)    
     country = models.CharField(max_length=15, blank=True, null=True)
     state = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=15, blank=True, null=True)
@@ -108,8 +107,8 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-    def full_Address(self):
-        return f'{self.address_line1},{self.address_line2}'
+    # def full_Address(self):
+    #     return f'{self.address_line1},{self.address_line2}'
 
     def __str__(self):
         return self.user.email
