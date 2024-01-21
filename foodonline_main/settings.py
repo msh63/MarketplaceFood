4317433929cdd7f825b1aa97629396003f09a5bd
msh63 <mohammad.shafe@gmail.com>
2024-01-21 17:30:14 +1300

    Restaurant Profile &Google Map

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor',
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -155,3 +156,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <mohammad.shafe@gmail.com>'
+
+GOOGLE_API_KEY = 'AIzaSyAenVBxFyAJigH0Ibd6Wv3yVhhYHofouzY'
