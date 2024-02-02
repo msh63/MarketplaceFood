fddd718d67614274950aa733b547657caa2a9df4
msh63 <mohammad.shafe@gmail.com>
2024-02-02 15:25:37 +1300

    Open Close Badge set

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,10 +1,11 @@
+from datetime import date
 from django.http import JsonResponse
 from django.shortcuts import render,get_object_or_404,redirect
 from .context_processors import get_cart_counter,get_cart_amounts
 from .models import Cart
 from menu.models import Category, FoodItem
 from django.db.models import Prefetch
-from vendor.models import Vendor
+from vendor.models import OpeningHour, Vendor
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
@@ -23,24 +24,35 @@ def marketplace(request):
         }
     return render(request,'marketplace/listings.html',context)
 
-def vendor_detail(request,vendor_slug):
-    vendor=get_object_or_404(Vendor,vendor_slug=vendor_slug)
-    categories=Category.objects.filter(vendor=vendor).prefetch_related(
+def vendor_detail(request, vendor_slug):
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
         Prefetch(
             'fooditems',
-            queryset=FoodItem.objects.filter(is_available=True)
+            queryset = FoodItem.objects.filter(is_available=True)
         )
     )
+
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
+    
+    # Check current day's opening hours.
+    today_date = date.today()
+    today = today_date.isoweekday()
+    
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
     if request.user.is_authenticated:
-        cart_items=Cart.objects.filter(user=request.user)
+        cart_items = Cart.objects.filter(user=request.user)
     else:
-        cart_items=None
-    context={
-        'vendor':vendor,
-        'categories':categories,
-        'cart_items':cart_items,
+        cart_items = None
+    context = {
+        'vendor': vendor,
+        'categories': categories,
+        'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
     }
-    return render(request,'marketplace/vendor_detail.html',context)
+    return render(request, 'marketplace/vendor_detail.html', context)
 
 
 def add_to_cart(request, food_id):
